62782aac4ff04517f6459d36362df979341fc823
Nux <138419122+Nuxlar@users.noreply.github.com>
2025-06-25 01:55:28 -0400

    1.0.0 release

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Remove Zoom Out
 
+Each effect change can be turned on/off through the config.
+
+- Reduces Volcanic Egg zoom out and raises the camera position slightly for better visibility.
+- Reduces Sojourn/Reprieve's zoom out when flying (Both Seeker Utilities).
+- Removes Unseen Hand's zoom out when aiming the ability (Seeker Secondary).
+- Removes Meridian Will's zoom out when aiming the ability (False Son Alt Utility).
--- RemoveZoomOut/Main.cs
+++ RemoveZoomOut/Main.cs
@@ -1,7 +1,7 @@
 using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
 using RoR2.ContentManagement;
-using System.Diagnostics;
-using System.IO;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -18,38 +18,71 @@ namespace RemoveZoomOut
     internal static Main Instance { get; private set; }
     public static string PluginDirectory { get; private set; }
 
+    public static ConfigEntry<bool> changeSeekerUtil;
+    public static ConfigEntry<bool> changeSeekerSec;
+    public static ConfigEntry<bool> changeFSUtil;
+    public static ConfigEntry<bool> changeEgg;
+    private static ConfigFile RZOConfig { get; set; }
+
     public void Awake()
     {
       Instance = this;
 
-      Stopwatch stopwatch = Stopwatch.StartNew();
-
       Log.Init(Logger);
-      // LoadAssets();
 
-      stopwatch.Stop();
-      Log.Info_NoCallerPrefix($"Initialized in {stopwatch.Elapsed.TotalSeconds:F2} seconds");
+      RZOConfig = new ConfigFile(Paths.ConfigPath + "\\com.Nuxlar.RemoveZoomOut.cfg", true);
+      changeEgg = RZOConfig.Bind<bool>("General", "Change Egg Zoom", true, "Reduces camera zoom out on the Volcanic Egg equipment.");
+      changeFSUtil = RZOConfig.Bind<bool>("General", "Change False Son Zoom", true, "Removes camera zoom out when aiming False Son's alt utility.");
+      changeSeekerSec = RZOConfig.Bind<bool>("General", "Change Seeker Sec Zoom", true, "Removes camera zoom out when aiming Seeker's secondary Unseen Hand.");
+      changeSeekerUtil = RZOConfig.Bind<bool>("General", "Change Seeker Util Zoom", true, "Removes camera zoom out when using Seeker's utility (both).");
+
+      LoadAssets();
+
+      if (changeSeekerSec.Value)
+        TweakEntityState(RoR2BepInExPack.GameAssetPaths.RoR2_DLC2_Seeker_EntityStates_Seeker.UnseenHand_asset, "abilityAimType", "3");
+      if (changeFSUtil.Value)
+        TweakEntityState(RoR2BepInExPack.GameAssetPaths.RoR2_DLC2_FalseSon_EntityStates_FalseSon.AimMeridiansWill_asset, "cameraTeleportPositionOffset", "0 0 -3");
+    }
+
+    private void TweakEntityState(string path, string fieldName, string value)
+    {
+      AssetReferenceT<EntityStateConfiguration> escRef = new AssetReferenceT<EntityStateConfiguration>(path);
+      AssetAsyncReferenceManager<EntityStateConfiguration>.LoadAsset(escRef).Completed += (x) =>
+      {
+        EntityStateConfiguration esc = x.Result;
+        for (int i = 0; i < esc.serializedFieldsCollection.serializedFields.Length; i++)
+        {
+          if (esc.serializedFieldsCollection.serializedFields[i].fieldName == fieldName)
+          {
+            esc.serializedFieldsCollection.serializedFields[i].fieldValue.stringValue = value;
+          }
+        }
+      };
     }
 
     private static void LoadAssets()
     {
-      // Example for how to properly load in assets to be used later
-      // AssetAsyncReferenceManager<Material>.LoadAsset(new AssetReferenceT<Material>(RoR2BepInExPack.GameAssetPaths.RoR2_Base_moon.matMoonTerrain_mat)).Completed += (x) => variableName = x.Result;
-    }
-
-    /*
-        private static void TweakAssets()
+      if (changeEgg.Value)
+      {
+        AssetReferenceT<GameObject> eggRef = new AssetReferenceT<GameObject>(RoR2BepInExPack.GameAssetPaths.RoR2_Base_FireBallDash.FireballVehicle_prefab);
+        AssetAsyncReferenceManager<GameObject>.LoadAsset(eggRef).Completed += (x) =>
         {
-          Example for how to edit an asset once it finishes loading
-          AssetAsyncReferenceManager<GameObject>.LoadAsset(new AssetReferenceT<Material>(RoR2BepInExPack.GameAssetPaths.RoR2_DLC2_Items_LowerPricedChests.PickupSaleStar_prefab)).Completed += delegate (AsyncOperationHandle<GameObject> obj)
-          {
-            MeshCollider collider = obj.Result.transform.find("SaleStar")?.GetComponent<MeshCollider>();
-            if (collider)
-            {
-              collider.convex = true;
-            }
-          };
-        }
-    */
+          GameObject vehicle = x.Result;
+          CameraTargetParams camParams = vehicle.GetComponent<CameraTargetParams>();
+          camParams.cameraParams.data.idealLocalCameraPos = new Vector3(0f, 2f, -15f);
+        };
+      }
+
+      if (changeSeekerUtil.Value)
+      {
+        AssetReferenceT<GameObject> soujournRef = new AssetReferenceT<GameObject>(RoR2BepInExPack.GameAssetPaths.RoR2_DLC2_Seeker_SojournVehicle.SojournVehicle_prefab);
+        AssetAsyncReferenceManager<GameObject>.LoadAsset(soujournRef).Completed += (x) =>
+        {
+          GameObject vehicle = x.Result;
+          CameraTargetParams camParams = vehicle.GetComponent<CameraTargetParams>();
+          camParams.cameraParams.data.idealLocalCameraPos = new Vector3(0f, 0f, -15f); // 0 -2.12 -20.78
+        };
+      }
+    }
   }
 }
